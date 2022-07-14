73a657b692ae1967b399b3ef6aeb7b35e9ef889f
Sohang Chopra <sohangchopra@gmail.com>
2022-07-14 19:35:22 +0530

    Show random card numbers

--- 00_Alternate_Languages/01_Acey_Ducey/Forth/acey-ducey.fs
+++ 00_Alternate_Languages/01_Acey_Ducey/Forth/acey-ducey.fs
@@ -1,3 +1,11 @@
+require random.fs
+
+: time ( -- t ; Get system time: https://www.rosettacode.org/wiki/System_time#Forth )
+    cputime d+ 1000 um/mod nip ;
+
+( time seed ! -- Init seed with timestamp NOT WORKING! )
+1234 seed !
+
 cr ." ACEY-DUCEY IS PLAYED IN THE FOLLOWING MANNER"
 cr ." THE DEALER (COMPUTER) DEALS TWO CARDS FACE UP"
 cr ." YOU HAVE AN OPTION TO BET OR NOT BET DEPENDING"
@@ -7,3 +15,8 @@ cr ." IF YOU DO NOT WANT TO BET, INPUT '0'"
 cr ." YOU NOW HAVE 100 DOLLARS."
 cr 
 cr ." HERE ARE YOUR NEXT TWO CARDS:"
+( TODO: Show face cards A,J,Q,K properly )
+cr 13 random 1+ .
+cr 13 random 1+ .
+cr
+cr ." WHAT IS YOUR BET?"
